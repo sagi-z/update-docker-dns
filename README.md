f31c211393d0f9d56d1103f1ec02834e46c5016d
Sagi Zeevi <sagi.zeevi@gmail.com>
2018-02-25 16:25:04 +0200

    prepare for github

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
 update-docker-dns
 =================
 
+Selectively expose docker containers by name.
+
+[Install 1/3] Copy the script to where it belongs
+-------------------------------------------------
+
 ```
 sudo cp update-docker-dns.sh /usr/loca/bin
 ```
 
-Add dnsmasq support
--------------------
+[Install 2/3] Add dnsmasq support
+---------------------------------
+
+* If you **have** a Network Manager (most linux desktops), and it is
+using *dnsmasq*:
 
 ```
+sudo bash -c "cat > /etc/NetworkManager/dnsmasq.d"
+addn-hosts=/etc/docker-container-hosts
+^D
+```
+
+```
+sudo systemctl restart network-manager.service
+```
+
+* If you **don't** have a Network Manager, or it is not using *dnsmasq*:
+
+```
+sudo apt install -y dnsmasq
 sudo bash -c "cat > /etc/dnsmasq.d/docker-dns"
 addn-hosts=/etc/docker-container-hosts
-#interface=docker0
 ^D
 ```
 
@@ -19,14 +39,14 @@ addn-hosts=/etc/docker-container-hosts
 sudo systemctl restart dnsmasq.service
 ```
 
-Register it as a system service
--------------------------------
+[Install 3/3] Register it as a system service
+---------------------------------------------
 
 ```
-sudo vim /etc/systemd/system/docker-dnsmasq.service
+sudo bash -c "cat > /etc/systemd/system/docker-dnsmasq.service"
 [Unit]
 Description=Update dnsmasq with selected docker containers
-After=docker.service dnsmasq.service
+After=docker.service dnsmasq.service network-manager.service
 
 [Service]
 ExecStart=/usr/local/bin/update-docker-dns.sh
@@ -34,6 +54,7 @@ Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
+^D
 ```
 
 ```
@@ -41,3 +62,7 @@ sudo systemctl enable docker-dnsmasq.service
 sudo systemctl start docker-dnsmasq.service
 ```
 
+Usage
+-----
+See [Install and test example](https://www.theimpossiblecode.com/blog/docker-expose-service/).
+See [Usage example](https://www.theimpossiblecode.com/blog/docker-wordpress-multisite-with-subdomains/).
--- update-docker-dns.sh
+++ update-docker-dns.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# update-docker-dns is used to selectively expose docker containers by name
+# Copyright (c) 2018, Sagi Zeevi (www.theimpossiblecode.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the organization nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY yourname ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL yourname BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 WAIT_EVENS_TO=10
 ECHO="/bin/echo"
 
@@ -88,18 +114,12 @@ trap timeout_handler SIGALRM
 
 # Exernal loop to handle docker restarts
 while [ 1 ] ; do
-    # Handle race conditions when dnsmasq is started before docker0 interface was up
     $ECHO "waiting for docker to be up"
     if ! docker network ls > /dev/null 2>&1 ; then
         # Wait for docker networks to start
         while ! docker network ls > /dev/null 2>&1 ; do
             sleep 1
         done
-        $ECHO "restarting dnsmasq"
-        systemctl restart dnsmasq
-    elif ! systemctl is-active dnsmasq.service > /dev/null ; then
-        # It probably failed if it came up before the docker0 device was created
-        systemctl start dnsmasq
     fi
 
     $ECHO "register for docker start/stop events"
